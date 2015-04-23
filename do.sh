adb61f3477e6cf49f26c13727b97f54625454ef0
osankur <osankur@gmail.com>
2015-04-23 10:10:56 +0200

    trying optimizations

---
+++ do.sh
@@ -0,0 +1,3 @@
+time python abssynthe.py -v LD -opt 1 ../bench-syntcomp14/amba5b5y.aag;
+time python abssynthe.py -v LD -opt 2 ../bench-syntcomp14/amba5b5y.aag;
+time python abssynthe.py -v LD -opt 3 ../bench-syntcomp14/amba5b5y.aag;
