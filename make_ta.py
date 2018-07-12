da4b3ed5324484edc119d2883982d1852db458b8
SANKUR Ocan <ocan.sankur@irisa.fr>
2018-07-12 18:01:16 +0200

    fixed urgency

--- make_ta.py
+++ make_ta.py
@@ -1,3 +1,18 @@
+"""
+Given an AIG file and a time configuration file TIME which contains a pair of integers for each latch. The TAWRITER class constructs a timed automaton which simulates this circuit with the following timing constraints.
+For each latch, we are given the pair (delay[0],delay[1]). The circuit simulation can perform a new cycle if only if for each latch l:
+- if l is about to change from 1 to 0, then at least delay[0] time units have passed since the value was changed to 1
+- if l is about to change from 0 to 1, then at least delay[1] time units have passed since the value was changed to 0
+If the value of l does not change, then there is no time constraint.
+
+The present version computes an urgent model where each cycle is executed as soon as possible.
+A deadlock is present iff there is a deadlock in the model (in the sense of Uppaal) or
+location "dead" is reachable.
+
+Latch names are obtained by replacing <, > characters by _ and by prefixing them by L
+Input names are obtained ..................................
+Clock names are obtained .................................. and by prefixing them by x_
+"""
 import sys
 import argparse
 import log
@@ -55,7 +70,8 @@ class NTA:
             temp.dump()
         print "<instantiation>{0}</instantiation>".format(self.inst)
         print "<system>{0}</system>".format(self.system)
-        # print "<queries><query><formula>", self.query, "</formula></query></queries>"
+        if self.query:
+            print "<queries><query><formula>", self.query, "</formula></query></queries>"
         print "</nta>"
 
 class Template:
@@ -139,23 +155,40 @@ class TAWRITER:
         self._cached_transition = None
         self.delays = self._read_delays(time_file_name)
 
+
     def _read_delays(self, time_file_name):
         delays = dict()
         latches = [x.lit for x in self.aig.iterate_latches()]
         with open(time_file_name,'r') as fp:
             for l in range(len(latches)):
                 s = fp.readline()
-                si = map(lambda x: int(x), s.split(" "))
-                assert(len(si) == 2)
-                delays[latches[l]] = (si[0],si[1])
+                if s <> "":
+                #print "Doing line:<{0}>".format(s)
+                #print s.split(" ")
+                    si = map(lambda x: int(x), s.split(" "))
+                    assert(len(si) == 2)
+                    delays[latches[l]] = (si[0],si[1])
+                else:
+                    delays[latches[l]] = (0,0)
         log.DBG_MSG("Latch delays: " + str(delays))
         return delays
 
     def set_lit2formula(self, lit, s):
         self.lit_to_formula[lit] = s
 
-    def clean_name(self, name):
-        return name.replace("<", "_").replace(">","_");
+    """ Name of stripped literal which must be an input or latch """
+    def name_of(self, lit):
+        assert(lit == strip_lit(lit))
+        (intput, latch, and_gate) = self.aig.get_lit_type(lit)
+        # is it an input, latch, gate or constant
+        #print >> sys.stderr, "lit", lit, (input,latch,and_gate)
+        if latch:
+            return "L"+self.aig.get_lit_name(lit).replace("<", "_").replace(">", "_");
+        elif input:
+            # Boolean variables are encoded as 1-bit integers for now
+            return "I"+self.aig.get_lit_name(lit).replace("<", "_").replace(">", "_");
+        else:
+            raise "And gates or constant inputs have no name"
 
     def lit2formula(self, lit):
         if lit in self.lit_to_formula:
@@ -163,19 +196,23 @@ class TAWRITER:
         # get stripped lit
         stripped_lit = strip_lit(lit)
         is_neg = lit_is_negated(lit)
-        (intput, latch, and_gate) = self.aig.get_lit_type(stripped_lit)
+        (input, latch, and_gate) = self.aig.get_lit_type(stripped_lit)
         # is it an input, latch, gate or constant
-        if intput or latch:
-            result = self.clean_name(self.aig.get_lit_name(stripped_lit))
+        if input or latch:
+            # Boolean variables are encoded as 1-bit integers for now
+            result = "({0} == 1)".format(self.name_of(stripped_lit))
         elif and_gate:
-            result = ("({0} &amp; {1})".format(self.lit2formula(and_gate.rhs0),
+            result = ("({0} &amp;&amp; {1})".format(self.lit2formula(and_gate.rhs0),
                       self.lit2formula(and_gate.rhs1)))
         else:  # 0 literal, 1 literal and errors
-            result = "false"
+            result = "false" # this means false
         # cache result
         self.lit_to_formula[stripped_lit] = result
         if is_neg:
-            result = "!({0})".format(result)
+            if result == "false":
+                result = "true"
+            else:
+                result = "!{0}".format(result)
             self.lit_to_formula[lit] = result
         return result
 
@@ -192,32 +229,28 @@ class TAWRITER:
     Given K, make timed automaton model that restricts each cycle to K time units.
     """
     def get_cycle_time_model(self, K):
+        latch_locations = dict()
         # clock name associated to given lit which is a latch
         clock_name = dict()
-        # input name associated to given lit which is an input
-        inputs = dict()
-        # latch name associated to given lit which is a latch
-        latches = dict()
-
-        latch_locations = dict()
         nta = NTA()
         temp = Template("Circuit")
         nta.add_template(temp)
         nta.set_system("Process = Circuit();\nsystem Process;")
         decl = StringIO.StringIO()
         init = Location("Init", urgent=True,initial=True)
+        deadlock = Location("dead")
+        # nta.set_query("E<>Process.dead")
         for x in self.aig.iterate_latches():
             clock_name[x.lit] = "x_" + str(x.lit)
             print >> decl, "clock {0};".format(clock_name[x.lit])
-        print >> decl, "clock t,T;"
+        print >> decl, "clock T;"
         last_location = init
         input_list = list(chain(self.aig.iterate_uncontrollable_inputs(),self.aig.iterate_controllable_inputs()))
         for i in input_list:
-            inputs[i.lit] = self.clean_name(self.aig.get_lit_name(i.lit))
-            print >> decl, "bool {0};".format(inputs[i.lit])
-            loc_after_i = Location("JustSet"+inputs[i.lit], urgent=True)
-            tr0 = Transition(last_location, loc_after_i, up="{0} := false".format(inputs[i.lit]))
-            tr1 = Transition(last_location, loc_after_i, up="{0} := true".format(inputs[i.lit]))
+            print >> decl, "bool {0};".format(self.name_of(i.lit))
+            loc_after_i = Location("JustSet"+self.name_of(i.lit), urgent=True)
+            tr0 = Transition(last_location, loc_after_i, up="{0} := 0".format(self.name_of(i.lit)))
+            tr1 = Transition(last_location, loc_after_i, up="{0} := 1".format(self.name_of(i.lit)))
             temp.add_transition(tr0)
             temp.add_transition(tr1)
             last_location = loc_after_i
@@ -225,49 +258,62 @@ class TAWRITER:
 
         latch_list = list(self.aig.iterate_latches())
         latch_num = len(latch_list)
+
         next_funcs = self.get_next_funcs()
 
         for x in latch_list:
-            latches[x.lit] = "L" + self.clean_name(self.aig.get_lit_name(x.lit))
-            print >> decl, "bool {0};".format(latches[x.lit])
-            latch_locations[x.lit] = Location("Updated"+ latches[x.lit], urgent=True)
+            print >> decl, "bool {0};".format(self.name_of(x.lit))
+            latch_locations[x.lit] = Location("Updated"+ self.name_of(x.lit), urgent=True)
 
-            # Add the following transitions
-            # up(l_i) ---- f(vec(l),I) = l_i, t=0, t:=0 ----> up(l_{i+1})
-            # up(l_i) ---- f(vec(l),I) = 0 && l_i = 1, x_i >= D_i^1, l_i := 0, t:=0, x_i := 0 ----> up(l_{i+1})
-            # up(l_i) ---- f(vec(l),I) = 1 && l_i = 0, x_i >= D_i^0, l_i := 1, t:=0, x_i := 0 ----> up(l_{i+1})
-            g = "{0} == {1} &amp;&amp; t == 0".format(latches[x.lit], next_funcs[x.lit])
-            tr = Transition(last_location, latch_locations[x.lit], guard=g, up="t:=0")
+            # Basically, we add the following transitions but use intermediary states
+            # with invariants so that all transitions are urgent
+            # updated(l_{i-1},urg) ---- f(vec(l),I) = l_i ----> updated(l_{i+1})
+            # updated(l_{i-1},urg) ---- f(vec(l),I) = 0 && l_i = 1, x_i >= D_i^1, l_i := 0, x_i := 0 ----> updated(l_{i},urg)
+            # updated(l_{i-1},urg) ---- f(vec(l),I) = 0 && l_i = 1, x_i < D_i^1, l_i := 0, x_i := 0 ----> will_become1_nurg(l_{i})
+            # symmetrically for when the new value is 1
+            # will_become1_nurg(l_{i},invar:x_i<=D_i^1) ---- x_i >= D_i^0, l_i := 1, x_i := 0 ----> updated(l_{i}, urg)
+
+            # if the value does not change:
+            g = "{0} == {1}".format(self.name_of(x.lit), next_funcs[x.lit])
+            tr = Transition(last_location, latch_locations[x.lit], guard=g)
             temp.add_transition(tr)
 
+            # if it changes but the delay is already respected, move on directly
+            g = "{0} &amp;&amp; {0} != {1} &amp;&amp; {2} &gt;= {3}".format(self.name_of(x.lit), next_funcs[x.lit], clock_name[x.lit], self.delays[x.lit][0])
+            temp.add_transition(Transition(last_location, latch_locations[x.lit], guard=g))
+            g = "!{0} &amp;&amp; {0} != {1} &amp;&amp; {2} &gt;= {3}".format(self.name_of(x.lit), next_funcs[x.lit], clock_name[x.lit], self.delays[x.lit][1])
+            temp.add_transition(Transition(last_location, latch_locations[x.lit], guard=g))
+
+            # if we have to wait to respect the delay go to specific wait state and wait precisely for the delay (matching invariant + guard)
             loc0 = Location(latch_locations[x.lit].name + "_becomes0")
             loc0.set_invariant("{0} &lt;= {1}".format(clock_name[x.lit], self.delays[x.lit][0]))
             loc1 = Location(latch_locations[x.lit].name + "_becomes1")
             loc1.set_invariant("{0} &lt;= {1}".format(clock_name[x.lit], self.delays[x.lit][1]))
 
-            g = "{0} &amp;&amp; {0} != {1}".format(latches[x.lit], next_funcs[x.lit])
+            g = "{0} &amp;&amp; {0} != {1} &amp;&amp; {2} &lt; {3}".format(self.name_of(x.lit), next_funcs[x.lit], clock_name[x.lit], self.delays[x.lit][0])
             tr = Transition(last_location, loc0, guard=g)
             temp.add_transition(tr)
 
-            up="{0}:=0, {1} := {2}".format(clock_name[x.lit], latches[x.lit], next_funcs[x.lit])
+            up="{0}:=0, {1} := {2}".format(clock_name[x.lit], self.name_of(x.lit), next_funcs[x.lit])
             g = "{0} &gt;= {1}".format(clock_name[x.lit], self.delays[x.lit][0])
             tr = Transition(loc0, latch_locations[x.lit], guard=g, up=up)
             temp.add_transition(tr)
 
-            g = "!{0} &amp;&amp; {0} != {1}".format(latches[x.lit], next_funcs[x.lit])
+            g = "!{0} &amp;&amp; {0} != {1} &amp;&amp; {2} &lt; {3}".format(self.name_of(x.lit), next_funcs[x.lit], clock_name[x.lit], self.delays[x.lit][1])
             tr = Transition(last_location, loc1, guard=g)
             temp.add_transition(tr)
 
-            up = "{0}:=0, {1} := {2}".format(clock_name[x.lit], latches[x.lit], next_funcs[x.lit])
+            up = "{0}:=0, {1} := {2}".format(clock_name[x.lit], self.name_of(x.lit), next_funcs[x.lit])
             g = "{0} &gt;= {1}".format(clock_name[x.lit], self.delays[x.lit][1])
             tr = Transition(loc1, latch_locations[x.lit], guard=g, up=up)
             temp.add_transition(tr)
 
             last_location = latch_locations[x.lit]
             pass
-
-        come_back= Transition(last_location, init, guard="T &lt;= {0}".format(str(K)), up="t:=0,T:=0")
+        go_deadlock=Transition(last_location, deadlock, guard="T &gt;{0}".format(str(K)))
+        come_back= Transition(last_location, init, guard="T &lt;= {0}".format(str(K)), up="T:=0")
         temp.add_transition(come_back)
+        temp.add_transition(go_deadlock)
         last_location = init
         nta.set_declaration(decl.getvalue())
         nta.dump()
